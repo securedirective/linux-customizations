1b5ddca7344a0c5eeb95eb43c15e2219255ccb1a
Kenneth Johnson <kenpilot@gmail.com>
2017-02-04 18:43:08 -0500

    Bug fixes

--- install.sh
+++ install.sh
@@ -3,15 +3,24 @@
 SCRIPT=`realpath $0`
 SCRIPTPATH=`dirname $SCRIPT`
 
-ln -fs $SCRIPTPATH/.nanorc ~ && echo "Installed .nanorc" || echo "Failed to install .nanorc"
-ln -fs $SCRIPTPATH/.tmux.conf ~ && echo "Installed .tmux.conf" || echo "Failed to install .tmux.conf"
+function create_symlink {
+  ln -fs $2 $1 && echo "Symlinked $1 to $2" || echo "Failed to symlink $1 to $2"
+}
+create_symlink ~/.nanorc $SCRIPTPATH/.nanorc
+create_symlink ~/.tmux.conf $SCRIPTPATH/.tmux.conf
 
 bashextra=". $SCRIPTPATH/.bashrc.extra"
 function install_bash {
   if [ -e $1 ]; then
-    (grep  -Fxq "$bashextra" "$1" || echo "$bashextra" >> "$bashconf") && echo "Installed .bashrc.extra" || return 1
+    if (grep  -Fxq "$bashextra" "$1" || echo "$bashextra" >> "$1"); then
+      echo "Included .bashrc.extra into $1"
+    else
+      echo "Failed to include .bashrc.extra into $1"
+      return 1
+    fi
   else
+    echo "Could not find $1"
     return 1
   fi
 }
-install_bash ~/.bashrc || echo "Failed to install .bashrc.extra"
+install_bash ~/.bashrc
