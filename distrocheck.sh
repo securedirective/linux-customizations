38df21d27e248345a611899f636d74c1a809401d
Kenneth Johnson <kenpilot@gmail.com>
2017-02-05 18:43:00 -0800

    Added FreeBSD to distrocheck.sh

--- distrocheck.sh
+++ distrocheck.sh
@@ -14,15 +14,19 @@
 # debian/raspbian
 # arch
 
-if [ -e "/etc/redhat-release" ]; then
+if uname -a | grep -q FreeBSD; then
+  echo -en "freebsd:\t"; uname -srKm
+elif [ -e "/etc/redhat-release" ]; then
   echo "redhat"
   if [ -e "/etc/centos-release" ]; then
-    echo -en "centos\t"; cat /etc/centos-release
+    echo -en "centos:\t"; cat /etc/centos-release
     exit
   fi
 elif [ -e "/etc/debian_version" ]; then
   echo "debian"
   if grep Ubuntu /etc/lsb-release >/dev/null; then
-    echo -en "ubuntu\t"; sed -n -e '/DISTRIB_DESCRIPTION=/ s/\w*=\"//;s/\"$//p' /etc/lsb-release
+    echo -en "ubuntu:\t"; sed -n -e '/DISTRIB_DESCRIPTION=/ s/\w*=\"//;s/\"$//p' /etc/lsb-release
   fi
+else
+  echo "unknown distro"
 fi
